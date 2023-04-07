0914e52790d3c40159b18d276668fbe287cf95d4
szhangCCNY <solomon.zhang12@gmail.com>
2023-04-06 22:44:15 -0400

    tested get json through button click

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+
+let jsonDatas;
+
+function buttonHandler(){
+  axios.get("http://localhost:8000/getdata").then(res => {
+    jsonDatas = res.data;
+    console.log(jsonDatas);
+  })
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={buttonHandler}>click me</button>
   );
 }
 
